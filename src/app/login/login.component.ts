ad31e0f65616fedff3c7bd075a173a7e5b565389
abdiel11-pulzar <abdielriveraortega@gmail.com>
2024-12-08 18:27:41 +0100

    Login interactivo

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,15 @@
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  state = LoginCompState.LOGIN;
   loginData = {
     email: '',
     password: ''
@@ -21,6 +22,12 @@ registerData = {
     email: '',
     password: ''
 };
+recoveryData = {
+  email: ''
+};
+onPasswordRecovery() {
+  console.log("Password Recovery Data:", this.recoveryData);
+}
 
 onLogin() {
     console.log("Login Data:", this.loginData);
@@ -29,4 +36,33 @@ onLogin() {
 onRegister() {
     console.log("Register Data:", this.registerData);
 }
+//state
+
+ForgotPassClick() {
+  this.state = LoginCompState.FORGOT_PASSWORD;
 }
+CreateAccountClick() {
+  this.state = LoginCompState.REGISTER;
+}
+LoginClick() {
+  this.state = LoginCompState.LOGIN;
+}
+
+isLoginState() {
+  return this.state == LoginCompState.LOGIN;
+}
+isRegisterState() {
+  return this.state == LoginCompState.REGISTER;
+}
+isForgotPasswordState() {
+  return this.state == LoginCompState.FORGOT_PASSWORD;
+}
+
+}
+export enum LoginCompState {
+  LOGIN,
+  REGISTER,
+  FORGOT_PASSWORD
+
+}
+
