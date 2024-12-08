7fbefce62d1e637dfca60154c82b7109cd92e5b3
abdiel11-pulzar <abdielriveraortega@gmail.com>
2024-12-08 18:35:46 +0100

    key de firebase agregado

---
+++ src/environment/environment prod.ts
@@ -0,0 +1,18 @@
+
+
+  export const environment = {
+    production: true,
+    firebaseConfig : {
+      apiKey: "AIzaSyCepG5wXhy2M7x8WPpV9ncpoPzIxxI3DkQ",
+      authDomain: "reds-67f4e.firebaseapp.com",
+      projectId: "reds-67f4e",
+      storageBucket: "reds-67f4e.firebasestorage.app",
+      messagingSenderId: "37108819357",
+      appId: "1:37108819357:web:20f035068ccf174dc33b74",
+      measurementId: "G-GHQ921Q27D"
+       
+      }
+    
+    
+    }
+  
