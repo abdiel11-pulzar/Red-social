037f57c5e9957d9622dd8a53b86449067b15e860
abdiel11-pulzar <abdielriveraortega@gmail.com>
2024-12-08 19:20:56 +0100

    Se muestra la info del usuario en el perfil

--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-perfil',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './perfil.component.html',
   styleUrl: './perfil.component.css'
 })
 export class PerfilComponent {
-
+  userData: any;
+  constructor(private authService: AuthService){
+  
+  this.authService.userData$.subscribe((data) => {
+    this.userData = data;
+    console.log('Datos recibidos en el componente:', this.userData);
+  });
+  
+}
 }
