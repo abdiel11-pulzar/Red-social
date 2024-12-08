39fc714ef7860d42c103806cab7b7100aa6a3591
abdiel11-pulzar <abdielriveraortega@gmail.com>
2024-12-08 18:58:55 +0100

    Componentes agregados: feed, eprfil, grupo, evetno

---
+++ src/app/grupo/grupo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grupo',
+  imports: [],
+  templateUrl: './grupo.component.html',
+  styleUrl: './grupo.component.css'
+})
+export class GrupoComponent {
+
+}
