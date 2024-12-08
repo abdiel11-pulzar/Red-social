8000ce5ad60b9dec57db62c2e7ad2d59989dccd4
abdiel11-pulzar <abdielriveraortega@gmail.com>
2024-12-08 19:08:33 +0100

    Diseño de feed agregado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { FeedComponent } from './feed/feed.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { EventoComponent } from './evento/evento.component';
+import { GrupoComponent } from './grupo/grupo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+
+    {path: "", component: FeedComponent},
+    {path: "perfil", component: PerfilComponent},
+    {path: "eventos", component: EventoComponent},
+    {path: "grupos", component: GrupoComponent},
+];
