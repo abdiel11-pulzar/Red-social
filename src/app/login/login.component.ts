a140cf6c9c43535c00f2c16e0fd618a10b266439
abdiel11-pulzar <abdielriveraortega@gmail.com>
2024-12-08 18:15:12 +0100

    Diseño de login, registr, y recuperar contraseña agregado

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,32 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginData = {
+    email: '',
+    password: ''
+};
 
+registerData = {
+    firstName: '',
+    lastName: '',
+    career: '',
+    email: '',
+    password: ''
+};
+
+onLogin() {
+    console.log("Login Data:", this.loginData);
+}
+
+onRegister() {
+    console.log("Register Data:", this.registerData);
+}
 }
