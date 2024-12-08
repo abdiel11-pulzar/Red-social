05b8ca9d82a37609896296823a0182ef7d9ea763
abdiel11-pulzar <abdielriveraortega@gmail.com>
2024-12-08 18:54:56 +0100

    Boton cerrar sesión agregado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { AuthService } from './auth.service';
 import { NgIf } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent, NgIf],
+  imports: [RouterOutlet, LoginComponent, NgIf, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
