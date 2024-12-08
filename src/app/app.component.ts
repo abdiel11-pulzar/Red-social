e8cdb0ed24e1b1bb63400c0460e50c110c9ef91d
abdiel11-pulzar <abdielriveraortega@gmail.com>
2024-12-08 18:48:30 +0100

    Registro y login exitoso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,24 @@ import { LoginComponent } from './login/login.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  isUserLoggedIn  : boolean = false;
   title = 'redSocial';
+
+  constructor(private authService: AuthService){
+    
+    this.subscribeToAuthStatus();
+
+  }
+
+ 
+  subscribeToAuthStatus() {
+    this.authService.userStatus$.subscribe((isLoggedIn) => {
+      this.isUserLoggedIn = isLoggedIn;
+      console.log('¿Usuario logueado?:', this.isUserLoggedIn);
+      if (isLoggedIn) {
+        const user = this.authService.getCurrentUser();
+        console.log('Usuario actual:', user?.email);
+      }
+    });
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -10,10 +11,13 @@ import { FormsModule, NgForm } from '@angular/forms';
 })
 export class LoginComponent {
   state = LoginCompState.LOGIN;
+  constructor(private authServide : AuthService){
+
+  }
   loginData = {
     email: '',
     password: ''
-};
+  };
 
 registerData = {
     firstName: '',
@@ -31,10 +35,22 @@ onPasswordRecovery() {
 
 onLogin() {
     console.log("Login Data:", this.loginData);
+    if(this.loginData.email && this.loginData.password){
+      this.authServide.login(this.loginData.email, this.loginData.password)
+    }else{
+      alert("Completa todos los campos")
+    }
 }
 
 onRegister() {
     console.log("Register Data:", this.registerData);
+    if(this.registerData.firstName && this.registerData.lastName && this.registerData.career
+      && this.registerData.email && this.registerData.password
+    ){
+      this.authServide.registro(this.registerData.email,this.registerData.password,this.registerData.firstName,this.registerData.lastName,this.registerData.career)
+    }else{
+      alert("Completa todos los campos")
+    }
 }
 //state
 
